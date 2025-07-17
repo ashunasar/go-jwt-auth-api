930b79027071d2d502842f9ecc77959a205285ca
Asim Siddiqui <asimsiddiqui20172017@gmail.com>
2025-07-17 22:11:00 +0530

    add uuid dependency and define User model with relevant fields

--- models/users.go
+++ models/users.go
@@ -1,1 +1,16 @@
 package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	Id           uuid.UUID `json:"id"`
+	Email        string    `json:"email"`
+	Password     string    `json:"password"`
+	RefreshToken string    `json:"refresh_token"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+}
