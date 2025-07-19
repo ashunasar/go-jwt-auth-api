776bb5bfe36a4395925bc5b2908a3eaa58b6c372
Asim Siddiqui <asimsiddiqui20172017@gmail.com>
2025-07-19 11:32:01 +0530

    feat: implement signup route with request body validation

--- routes/routes.go
+++ routes/routes.go
@@ -1,13 +1,38 @@
 package routes
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+)
+
+type SignUpBody struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
 
 func Routes() *http.ServeMux {
 
 	router := http.NewServeMux()
 
+	router.HandleFunc("POST /api/auth/signup", func(w http.ResponseWriter, r *http.Request) {
+
+		var signUpBody SignUpBody
+
+		err := json.NewDecoder(r.Body).Decode(&signUpBody)
+		if errors.Is(err, io.EOF) {
+
+			w.Write([]byte("Empty Body Provided"))
+			return
+		}
+
+		w.Write([]byte("from post api"))
+
+	})
+
 	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello world!"))
+		w.Write([]byte("Hello world Bro !"))
 	})
 
 	return router
